39a07fe2b97f59ec9857567f9cd901624c81a2e2
Narawich2013 <narawich2013@gmail.com>
2021-04-30 22:57:31 +0700

    sdsd

--- src/Pages/Matching/Matching.js
+++ src/Pages/Matching/Matching.js
@@ -18,8 +18,8 @@ function Matching(){
     </div>
     
     <div className="flex justify-center">
-    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold  m-1 py-2 px-4 border-b-2 border-blue-700 hover:border-blue-500 rounded focus:bg-yellow-600 group-focus:text-yellow-300  ">TOPS</button>
-    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold m-1 py-2 px-4 border-b-2 border-blue-700 hover:border-blue-500 rounded focus:bg-yellow-600 group-focus:text-yellow-300 ">PANTS</button>
+    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold  m-2 py-2 px-4 border-b-2 border-blue-700 hover:border-blue-500 rounded focus:bg-yellow-600 group-focus:text-yellow-300  ">TOPS</button>
+    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold m-2 py-2 px-4 border-b-2 border-blue-700 hover:border-blue-500 rounded focus:bg-yellow-600 group-focus:text-yellow-300 ">PANTS</button>
     </div>
 
         </div>
